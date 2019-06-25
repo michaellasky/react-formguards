0abdac107064253c41ca26c955144b6ed609b333
mlasky <mlasky@nuclearhorsestudios.com>
2019-06-24 23:55:44 -0400

    Fixed bug for formguards watching multiple elements
    Previously if a formguard was watching elements A, B, and C, and the
    user interacted with one, say B, then elements A and C would not be
    marked as dirty and therefor would not show as invalid.

--- src/formGuard.js
+++ src/formGuard.js
@@ -11,18 +11,22 @@ const FormGuard = ({
   value
 }) => {
   const isvalid = !!validatesWith.apply(null, value);
-  let isDirty = false;
 
   watches = asArray(watches);
-  watches.forEach(watch => {
-    const st = state[watch];
+  const isDirty = watches.reduce((isDirty, name) => {
+    const st = state[name];
+
     const markValid = isvalid && st && st.isvalid === undefined;
     const invalidate = !isvalid && st && st.isvalid !== false;
 
-    if (invalidate || markValid) { mergeState(watch, { isvalid }); }
-    if (st.updating)             { mergeState(watch, { updating: false }); }
+    if (invalidate || markValid) { mergeState(name, { isvalid }); }
+    if (st.updating)             { mergeState(name, { updating: false }); }
 
-    isDirty = isDirty || (st && st.dirty);
+    return isDirty || (st && st.dirty);
+  }, false);
+
+  isDirty && watches.forEach(name => {
+    if (!state[name].dirty) { mergeState(name, { dirty: true }); }
   });
 
   return !isvalid && isDirty === true &&
